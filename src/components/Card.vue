3ccd7a4b0c980889e4be96ba9befe12b185c2013
Dhanvinesh K <75219613+Reaper-Dhan@users.noreply.github.com>
2022-02-10 01:05:51 +0530

    Add files via upload

---
+++ src/components/Card.vue
@@ -0,0 +1,110 @@
+<script>
+import { computed } from 'vue'
+
+export default {
+  props: {
+    matched: {
+      type: Boolean,
+      default: false
+    },
+    position: {
+      type: Number,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, context) {
+    const flippedStyles = computed(() => {
+      if (props.visible) {
+        return 'is-flipped'
+      }
+    })
+
+    const selectCard = () => {
+      context.emit('select-card', {
+        position: props.position,
+        faceValue: props.value
+      })
+    }
+
+    return {
+      flippedStyles,
+      selectCard
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card" :class="flippedStyles" @click="selectCard">
+    <div class="card-face is-front">
+      <img
+        class="card-image"
+        :srcset="`/images/${value}@2x.png 2x, /images/${value}.png 1x`"
+        :src="`/images/${value}.png`"
+        :alt="value"
+      />
+      <img v-if="matched" src="/images/checkmark.svg" class="icon-checkmark" />
+    </div>
+    <div class="card-face is-back"></div>
+  </div>
+</template>
+
+<style>
+.card {
+  position: relative;
+  transition: 0.5s transform ease-in;
+  transform-style: preserve-3d;
+}
+
+.card.is-flipped {
+  transform: rotateY(180deg);
+}
+
+.card-face {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  backface-visibility: hidden;
+}
+
+.card-face.is-front {
+background: rgba( 0, 2, 148, 0.8 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 8.5px );
+-webkit-backdrop-filter: blur( 8.5px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );  color: white;
+  transform: rotateY(180deg);
+}
+
+.card-face.is-back {
+background: rgba( 133, 206, 255, 0.8 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 8.5px );
+-webkit-backdrop-filter: blur( 8.5px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );  color: white;
+}
+
+.card-image {
+  max-width: 100%;
+}
+
+.icon-checkmark {
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
+}
+</style>
---
+++ src/components/NewGameButton.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  props: {
+    newPlayer: {
+      type: Boolean,
+      default: true
+    }
+  },
+  setup(props, ctx) {
+    const startNewGame = () => {
+      ctx.emit('start-new-game')
+    }
+
+    return {
+      startNewGame
+    }
+  }
+}
+</script>
+
+<template>
+  <br><br><br>
+  <center>
+      <h1>Welcome to Memory-Card Game</h1>
+  </center>
+  <br><br>
+  <button v-if="newPlayer" @click="startNewGame" :class="$style.button">Start Game</button>
+  <button v-else @click="startNewGame" :class="$style.button">Restart Game</button>
+  <br>
+</template>
+
+<style module>
+h1 {
+  font-family: 'Permanent Marker', cursive;
+  color:rgb(0, 0, 0); width: 400px;
+  font-size: 40px;
+}
+.button {
+  background-color: #fffb00;
+  color: black;
+  padding: 8px 16px 10px;
+  display: flex;
+  align-items: center;
+  margin: 0 auto 30px;
+  font-weight: bold;
+  font-family: 'Titillium Web', sans-serif;
+  font-weight: bold;
+  font-size: 1.2rem;
+  border: 0;
+  border-radius: 10px;
+  transition: 0.2s all ease-in;
+  border: 5px solid #52f8f8;
+}
+
+.button:hover {
+  border: 2px solid #ff07c1;
+  background-color: #52f8f8;
+  color: black;
+  transform: translateY(-10px);
+}
+</style>
